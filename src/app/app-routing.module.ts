0fe7cde57922acda29c38b69dcdf06e643d20082
Meece <meece.work@gmail.com>
2022-11-10 22:14:54 +0200

    Added routes to routing module
    Increased card width

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardsContainerComponent } from './cards-container/cards-container.component';
 
 const routes: Routes = [
-  
+  { path: ':id', component: CardsContainerComponent },
+  { path: '**', redirectTo: '/1', pathMatch: 'full' }
 ];
 
 @NgModule({
